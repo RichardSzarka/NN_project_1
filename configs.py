e2ce6135c9d459460e0026bed348e7b775255a23
RichardSzarka <xszarkar@stuba.sk>
2024-03-19 16:11:23 +0100

    final commit

--- configs.py
+++ configs.py
@@ -1,27 +1,37 @@
 from pydantic import BaseModel
 import torch
-from torch.optim import SGD
 
 
 class Parameters(BaseModel):
+    # Dataset parameters:
+    binary: bool = True
+    balance_binary: bool = False
+
+    # Training parameters:
     lr: float = 6e-3
     epochs: int = 10000
     batch_size: int = 32
-    binary: bool = True
-    balance_binary: bool = False
-    batch_norm: bool = True
     patience: int = 150
     device: str = "cuda" if torch.cuda.is_available() else "cpu"
+
+    # Model parameters:
+    batch_norm: bool = True
+
     model_config: dict = {"arbitrary_types_allowed": True}
 
 
 class Parameters_sweep(BaseModel):
+    # Dataset parameters for hyperparameter sweeping:
+    binary: dict = {"values": [0]}
+    balance_binary: dict = {"values": [0]}
+
+    # Training parameters for hyperparameter sweeping:
     lr:dict = {"min": 0.00001, "max": 0.01}
     epochs: dict = {"values": [10000]}
     batch_size: dict = {"values": [8, 16, 32, 64, 128]}
-    binary: dict = {"values": [0]}
-    balance_binary: dict = {"values": [0]}
-    batch_norm: dict = {"values": [1, 0]}
     patience: dict = {"values": [150]}
     device: dict = {"values": ["cuda" if torch.cuda.is_available() else "cpu"]}
+
+    # Model parameters for hyperparameter sweeping:
+    batch_norm: dict = {"values": [1, 0]}
     model_config: dict = {"arbitrary_types_allowed": True}
